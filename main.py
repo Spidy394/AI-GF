6823a521cd5a8416b5e9b968f712fea0ac9a9abf
Spidy394 <79023855+Spidy394@users.noreply.github.com.>
2025-05-24 11:43:18 +0530

    Continued converstion implemented

--- main.py
+++ main.py
@@ -4,22 +4,29 @@ from src.gemini_tts import gemini_speak_text
 
 def main():
     r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        audio_text = r.listen(source)
-        print("wait...")
 
-        try:
-            transcript = r.recognize_google(audio_text)
-            print("Himu: " + transcript)
-            result = call_gemini(transcript)
-            if result:
-                reply = result['candidates'][0]['content']['parts'][0]['text']
-                print("Rupa: " + reply)
-                gemini_speak_text(reply) 
-                
-        except Exception as e:
-            print("Sorry, I did not get that:", str(e))
+    while True:
+        with sr.Microphone() as source:
+            print("Listening...")
+            audio_text = r.listen(source)
+            print("wait...")
+
+            try:
+                transcript = r.recognize_google(audio_text)
+                print("Himu: " + transcript)
+                result = call_gemini(transcript)
+                if result:
+                    reply = result['candidates'][0]['content']['parts'][0]['text']
+                    print("Rupa: " + reply)
+                    gemini_speak_text(reply) 
+                    
+            except Exception as e:
+                print("Sorry, I did not get that:", str(e))
+
+        if transcript.lower() == "goodbye" or transcript.lower() == "bye":
+            print("Sayonara!")
+            break
+
 
 
 if __name__ == "__main__":
